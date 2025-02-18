d1f467cecaab372662be5022c867940828c4041c
msn2022 <93134182+nazari63@users.noreply.github.com>
2025-02-18 19:39:27 +0330

    Add files via upload

---
+++ Decentralized Sequencer.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract DecentralizedSequencer {
+    address public admin;
+    address[] public validators;
+    mapping(address => bool) public isValidator;
+
+    event TransactionSequenced(address indexed sender, bytes data);
+
+    constructor(address[] memory _validators) {
+        admin = msg.sender;
+        for (uint i = 0; i < _validators.length; i++) {
+            validators.push(_validators[i]);
+            isValidator[_validators[i]] = true;
+        }
+    }
+
+    function addValidator(address validator) external {
+        require(msg.sender == admin, "Only admin can add validators");
+        validators.push(validator);
+        isValidator[validator] = true;
+    }
+
+    function sequenceTransaction(bytes calldata data) external {
+        require(isValidator[msg.sender], "Only validators can sequence transactions");
+        emit TransactionSequenced(msg.sender, data);
+    }
+}
